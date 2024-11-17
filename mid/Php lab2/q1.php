2a395e4c0a03dafa6610cb8fe899912eb4faf4c9
Ashraful Islam Opu <ashrafulislamopu20@gmail.com>
2024-11-17 11:59:10 +0600

    Add files via upload

---
+++ mid/Php lab2/q1.php
@@ -0,0 +1,8 @@
+<?php
+    $length=8;
+    $width=5;
+    $area=($length*$width);
+    echo "The area of the rectangle is ".$area."\r\n";
+    $perimeter=2*($length+$width);
+    echo "The perimeter of the rectangle is ".$perimeter."\r\n";
+?>
---
+++ mid/Php lab2/q2.php
@@ -0,0 +1,6 @@
+<?php
+$amount = 10000;
+$vat_percent = 15;
+$vat_amount = ($amount * $vat_percent) / 100;
+echo "VAT for taka " . $amount . " is " . $vat_amount . " taka."
+    ?>
