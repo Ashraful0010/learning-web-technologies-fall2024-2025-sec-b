273aa042689ac83580df80eb61243ce3d77e5818
Ashraful Islam Opu <ashrafulislamopu20@gmail.com>
2024-11-17 11:57:58 +0600

    Delete mid/Lab5 directory

--- mid/Lab5/q1.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $length=8;
-    $width=5;
-    $area=($length*$width);
-    echo "The area of the rectangle is ".$area."\r\n";
-    $perimeter=2*($length+$width);
-    echo "The perimeter of the rectangle is ".$perimeter."\r\n";
-?>
